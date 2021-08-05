efa25c681192638cd8d97c7907741c9869101840
Sam Murray <smurray@gonitro.com>
2021-08-05 17:46:35 +0200

    fix

--- ci/ci.bash
+++ ci/ci.bash
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-PLATFORM=$1
-ARCH=$2
-
 mkdir -p work
 cd work
 
-if [[ $PLATFORM == "linux" ]]; then
-  cmake .. -DCMAKE_BUILD_TYPE=Release
-fi
-
-
+cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build . --config Release --target install
