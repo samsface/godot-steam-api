d836a7739b506b9bd153ec9b4b3231003d9ed7c2
hiulit <hiulit@gmail.com>
2023-02-28 21:33:34 +0100

    Formatted code

--- addons/steam_api/settings.gd
+++ addons/steam_api/settings.gd
@@ -1,5 +1,5 @@
 extends Resource
 
-export(bool) var disable:bool
-export(String) var app_id:String
-export(String) var sdk_dir:String
+export(bool) var disable: bool
+export(String) var app_id: String
+export(String) var sdk_dir: String
